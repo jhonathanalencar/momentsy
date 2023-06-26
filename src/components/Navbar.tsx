425a8dead9b1928020673b26e499e3134a905736
jhonathanalencar <jhonathan-alencar@hotmail.com>
2023-06-26 16:33:47 -0300

    feat: change navbar background on the home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,26 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
 export function Navbar() {
+  const pathname = usePathname();
+
+  const isHomePage = pathname === '/';
+
+  const navbarBackgroundClasses = isHomePage
+    ? 'bg-transparent'
+    : 'bg-momentsy-red';
+
   return (
-    <header className="h-16 bg-momentsy-red px-4 md:px-8">
+    <header
+      className={`fixed top-0 z-10 h-16 w-full px-4 md:px-8 ${navbarBackgroundClasses}`}
+    >
       <div className="mx-auto flex h-full w-full max-w-[1440px] items-center">
         <nav>
-          <span className="text-2xl font-bold text-momentsy-beige">
+          <Link href="/" className="text-2xl font-bold text-momentsy-beige">
             Momentsy
-          </span>
+          </Link>
         </nav>
       </div>
     </header>
